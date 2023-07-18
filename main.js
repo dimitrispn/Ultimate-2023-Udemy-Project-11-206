6eaa720881973dd6be7003a3971aba5807d4247a
dimitrispn <dimitrios@tutamail.com>
2023-07-18 20:40:19 +0300

    Random Planets

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const changePlanet = document.getElementById("planet")
 
 
-
 changePlanet.addEventListener("click", () =>{
-     fetch("https://swapi.dev/api/planets/3/")
+    const randomPlanet = Math.ceil(Math.random()*10)
+     fetch(`https://swapi.dev/api/planets/${randomPlanet}/`)
         .then(response => response.json())
         .then(planet =>{
             console.log(planet);
