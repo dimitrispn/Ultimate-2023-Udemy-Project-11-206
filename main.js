40d16bd5342c8f1fc0570b7f27bedd036183e672
dimitrispn <dimitrios@tutamail.com>
2023-07-19 12:42:51 +0300

    Button render random planets

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const changePlanet = document.getElementById("planet")
-
+const planetName = document.querySelector("#planetName")
 
 changePlanet.addEventListener("click", () =>{
-    const randomPlanet = Math.ceil(Math.random()*10)
+    const randomPlanet = Math.ceil(Math.random()*60)
      fetch(`https://swapi.dev/api/planets/${randomPlanet}/`)
         .then(response => response.json())
         .then(planet =>{
-            console.log(planet);
+            // console.log(planet);
+            planetName.innerHTML = planet.name
+            // console.log(changePlanet);
+            // changePlanet.innerHTML = planet.name
+            
         })
 })
